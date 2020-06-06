2aa8b3d106e5a008d2f09e55b4c190bf579e3f43
diego0020 <gus0020@gmail.com>
2020-06-06 16:00:11 -0500

    add real time database

--- wave-fe/src/app/app.component.ts
+++ wave-fe/src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wave-fe';
   value = 42;
+
+  ngOnInit() {
+    console.log('init');
+    const starCountRef = firebase.database().ref('games/IyN3LKwbM5SKrXzC5Lnz/guess');
+    starCountRef.on('value', (snapshot) => {
+      console.log(snapshot);
+      this.value = snapshot.val();
+    });
+  }
 }
--- wave-fe/src/main.ts
+++ wave-fe/src/main.ts
@@ -4,9 +4,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+
+
 if (environment.production) {
   enableProdMode();
 }
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyALrxgUY5ce40Bqdd2xo_I16D3Qab8qf_Q',
+  authDomain: 'wavesync-6ac09.firebaseapp.com',
+  databaseURL: 'https://wavesync-6ac09.firebaseio.com',
+  projectId: 'wavesync-6ac09',
+  storageBucket: 'wavesync-6ac09.appspot.com',
+  messagingSenderId: '423781350664',
+  appId: '1:423781350664:web:e0449449bf4521626c00bd',
+  measurementId: 'G-XXL76TRHFD'
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
